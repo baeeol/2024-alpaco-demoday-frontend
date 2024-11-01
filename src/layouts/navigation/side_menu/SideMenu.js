cde5163720e484f432483c4ab4e9511d6e6de3f5
excutetype <qata163753@gmail.com>
2024-11-02 03:50:06 +0900

    feat: 로그아웃 버튼

--- src/layouts/navigation/side_menu/SideMenu.js
+++ src/layouts/navigation/side_menu/SideMenu.js
@@ -3,9 +3,11 @@ import MenuIcon from "./menu_icon/MenuIcon";
 import SmartToyIcon from "@mui/icons-material/SmartToy";
 import ChatIcon from "@mui/icons-material/Chat";
 import HelpCenterIcon from "@mui/icons-material/HelpCenter";
+import LogoutIcon from "@mui/icons-material/Logout";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import styles from "./SideMenu.module.css";
+import { getCookie, setCookie } from "lib/Cookie";
 
 function SideMenu({ isOpen, onClose }) {
   const location = useLocation();
@@ -21,6 +23,15 @@ function SideMenu({ isOpen, onClose }) {
           <MenuIcon Icon={SmartToyIcon} title="챗봇" url={"/"} />
           <MenuIcon Icon={ChatIcon} title="채팅방" url={"/group-chat"} />
           <MenuIcon Icon={HelpCenterIcon} title="Q&A" url={"/question"} />
+          <button
+            style={{ border: "none", background: "none", outline: "none" }}
+            onClick={() => {
+              setCookie("access-token", null);
+              window.location.reload();
+            }}
+          >
+            <MenuIcon Icon={LogoutIcon} title="로그아웃" />
+          </button>
         </Box>
       </div>
     </Drawer>
